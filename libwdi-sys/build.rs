013a300897bd8b3454baa3db68dc2f918240e449
Jędrzej Boczar <yendreij@gmail.com>
2023-03-20 14:46:57 +0100

    Add initial libwdi raw ffi bindings

---
+++ libwdi-sys/build.rs
@@ -0,0 +1,48 @@
+use std::{process::Command, path::PathBuf, env};
+
+fn root_dir() -> PathBuf {
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    dir.into()
+}
+
+fn generate_ffi_bindings() {
+    let header = root_dir().join("libwdi/libwdi/libwdi.h")
+        .into_os_string().into_string().unwrap();
+
+    let bindings = bindgen::Builder::default()
+        .allowlist_function("^wdi_.*")
+        .allowlist_type("^wdi_.*")
+        .allowlist_var("^wdi_.*")
+        .default_enum_style(bindgen::EnumVariation::ModuleConsts)
+        .header(header)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate libwdi bindings");
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_dir.join("bindings.rs"))
+        .expect("Could not write bindings file");
+}
+
+fn build_library() {
+    let src_dir = root_dir().join("libwdi");
+    let out_dir = root_dir().join("libwdi/x64/Release/lib");
+
+    // Build libwdi using microsoft build tools:
+    // MSBuild.exe libwdi.sln -p:Configuration=Release -p:Platform=x64
+    Command::new("MSBuild.exe")
+        .current_dir(src_dir)
+        .args(["libwdi.sln", "-p:Configuration=Release", "-p:Platform=x64"])
+        .status()
+        .expect("Failed to build libwdi using MSBuild");
+
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
+    println!("cargo:rustc-link-lib=static=libwdi");
+    // No "rerun-if-changed", just run msbuild every time as it does it's own cacheing.
+}
+
+fn main() {
+    generate_ffi_bindings();
+    build_library();
+}
