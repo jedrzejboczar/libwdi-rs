1dae45d2d7441b320103334c4827c02fa8d44b71
Jędrzej Boczar <yendreij@gmail.com>
2023-03-28 13:29:39 +0200

    feat(misc): add missing functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ mod enums;
 mod misc;
 
 pub use enums::{Error, Result, LogLevel, DriverType};
-pub use misc::{get_vendor_name, get_wdf_version, set_log_level, register_logger, unregister_logger};
+pub use misc::*;
 pub use crate::core::*;
--- src/misc.rs
+++ src/misc.rs
@@ -1,8 +1,10 @@
-use std::ffi;
+use std::{ffi, ptr};
 
 use libwdi_sys as wdi;
 
-use crate::enums::{LogLevel, check_error, Result};
+use crate::enums::{LogLevel, check_error, Result, DriverType};
+
+pub struct DriverInfo(pub wdi::tagVS_FIXEDFILEINFO);
 
 pub fn get_vendor_name(vid: u16) -> Option<&'static str> {
     let name = unsafe { wdi::wdi_get_vendor_name(vid) };
@@ -19,6 +21,31 @@ pub fn get_wdf_version() -> std::os::raw::c_int {
     unsafe {
         wdi::wdi_get_wdf_version()
     }
+}
+
+/// Returns `Some` if driver is supported. For WinUsb/LibUsb0/LibUsbK the info structure is filled,
+/// otherwise it is zeroed.
+pub fn is_driver_supported(driver_type: DriverType) -> Option<DriverInfo> {
+    let mut info: wdi::tagVS_FIXEDFILEINFO = unsafe { std::mem::zeroed() };
+    let is_supported = unsafe {
+        wdi::wdi_is_driver_supported(driver_type.to_ffi(), &mut info as *mut wdi::tagVS_FIXEDFILEINFO) != 0
+    };
+    if is_supported {
+        Some(DriverInfo(info))
+    } else {
+        None
+    }
+}
+
+/// This may fail during string conversion
+pub fn is_file_embedded(path: Option<&str>, name: &str) -> Result<bool> {
+    let path = path.map(ffi::CString::new).transpose()?;
+    let name = ffi::CString::new(name)?;
+    let path = path.map_or(ptr::null(), |s| s.as_ptr());
+    let result = unsafe {
+        wdi::wdi_is_file_embedded(path as *const i8, name.as_ptr() as *const i8)
+    };
+    Ok(result != 0)
 }
 
 pub fn set_log_level(level: LogLevel) -> Result<()> {
